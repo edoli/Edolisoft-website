80d5f5e3afe2f0d27f006eca6d26f06adb7ac4a2
daniel <edoli.mia@gmail.com>
2014-12-14 17:19:55 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(function() {
+  var route = window.location.href.match('/([^.^/]+)[^/]*$')[1];
+  console.log(route)
 
+
+  $('.navbar-nav li').removeClass('active');
+  $('.navbar-nav li#' + route).addClass('active');
 
 });
