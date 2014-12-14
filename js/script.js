4a39ef82b27bab1aaa33dd28c6e91b12893fa653
daniel <edoli.mia@gmail.com>
2014-12-14 16:13:32 +0900

    route

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-$(function(){
-  $("#content").load("html/main.html");
+$(function() {
+  var url = window.location.href;
+
+  var route = '';
+  var hashMatch = url.match('#(.+)$')
+  if (hashMatch) {
+    var hash = hashMatch[1];
+    route = hash.match('/([^/^#]+)$')[1];
+  }
+
+  if (route == '') {
+    route = 'main';
+  }
+
+  $("#content").load('html/' + route + '.html');
 });
